a09b74aeefe16ea1dff4d7b0f7ea4f2a837102ab
Ijbar Salah Eddine <ijbarbusiness@gmail.com>
2023-12-10 14:17:57 +0000

    Salah

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" imported modules """
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """ The class methods and attributes """
+    state_id = ""
+    name = ""
